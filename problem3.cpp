ce49e4d58560216cb067742f5fbda77bb8665bb2
Abdulrahman Reda <143408674+abdulrahmanreda53@users.noreply.github.com>
2026-02-28 22:42:02 +0200

    Add files via upload

---
+++ problem3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void printnum(int n)
+{
+    if (n==0)
+        return;
+
+    cout<<n<<endl;
+     printnum(n-1);
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    printnum(n);
+}
